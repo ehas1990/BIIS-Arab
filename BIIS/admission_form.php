17fc9aaa69df72bcd564d7b14fa2c88c30df8a69
ehas <ehasahamed@gmail.com>
2024-04-04 11:29:22 +0530

    admission format design

--- BIIS/admission_form.php
+++ BIIS/admission_form.php
@@ -1,34 +1,43 @@
 <?php
 
-$studentname = $_POST['studentname'];
-$academic = $_POST['academic'];
-$fathername = $_POST['fathername'];
-$mothername = $_POST['mothername'];
-$Contact = $_POST['contact']; // Corrected: changed to 'contact'
-$Whatsapp = $_POST['Whatsapp'];
-$dob = $_POST['dob'];
-$aadhar = $_POST['aadhar'];
-$address = $_POST['address'];
-$van = $_POST['van'];
-$boardarea = $_POST['boardarea'];
-$Distance = $_POST['Distance'];
-$covid = $_POST['covid'];
-$Madrassa = $_POST['Madrassa'];
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $studentname = $_POST['studentname'];
+    $academic = $_POST['academic'];
+    $fathername = $_POST['fathername'];
+    $mothername = $_POST['mothername'];
+    $contact = $_POST['contact']; // Corrected variable name
+    $Whatsapp = $_POST['Whatsapp']; // Corrected variable name
+    $dob = $_POST['dob'];
+    $aadhar = $_POST['aadhar'];
+    $address = $_POST['address'];
+    $van = $_POST['van'];
+    $boardarea = $_POST['boardarea'];
+    $Distance = $_POST['Distance']; // Corrected variable name
+    $covid = $_POST['covid'];
+    $Madrassa = $_POST['Madrassa']; // Corrected variable name
 
-$subject = "Admission Of Year " . $academic . " - Billiards International Islamic School"; // Corrected: Concatenation adjusted
-$to = "info@billiardsinternationalschool.com , ehasalpha@gmail.com";
-$txt = "Student Name : " . $studentname . "\r\n Academic Class : " . $academic . "\r\n Father Name : " . $fathername . "\r\n Mother Name: " . $mothername . "\r\n Contact Number: " . $Contact . "\r\n Whatsapp Number :" . $Whatsapp . "\r\n Date Of Birth : " . $dob . "\r\n Aadhar No: " . $aadhar . "\r\n Address : " . $address . "\r\n Van : " . $van . "\r\n Boarding Area :" . $boardarea . "\r\n Distance : " . $Distance . " \r\n Covid :" . $covid . " \r\n Madrassa Year: " . $Madrassa; // Corrected: Removed extra quote
-$subject = " $subject"; // Corrected: Removed extra space
-$headers = "From: " . $to; // Corrected: Changed from $email to $to
+    // Prepare email content
+    $subject = "Admission Of Year $academic - Billiards International Islamic School";
+    $to = "info@billiardsinternationalschool.com, ehasalpha@gmail.com"; // Separate multiple email addresses with commas
+    $txt = "Student Name: $studentname\r\nAcademic Class: $academic\r\nFather Name: $fathername\r\nMother Name: $mothername\r\nContact Number: $contact\r\nWhatsapp Number: $Whatsapp\r\nDate Of Birth: $dob\r\nAadhar No: $aadhar\r\nAddress: $address\r\nNeed a School Van Service ?: $van\r\nVehicle Boarding Area: $boardarea\r\nDistance between House to School: $Distance\r\nHas Covid been vaccinated?: $covid\r\nMadrassa Year: $Madrassa";
+    $headers = "From: info@billiardsinternationalschool.com"; // Set from email address
 
-if (mail($to, $subject, $txt, $headers)) {
-    // If email sent successfully, set JavaScript alert and redirect to index.html
-    echo '<script> window.location.href = "index.html";alert("Message sent successfully!");</script>';
-    exit(); // Make sure to exit after redirecting
+
+    // Attempt to send email
+    if (mail($to, $subject, $txt, $headers)) {
+        // If email sent successfully, redirect to index.html
+        echo '<script>window.location.href = "index.html";alert("Message sent successfully!");</script>';
+        exit(); // Make sure to exit after redirecting
+    } else {
+        // If email failed to send, display error message
+        $alert_message = "Something went wrong";
+        $alert_type = "failure";
+    }
 } else {
-    // If email failed to send, return error message
-    $alert_message = "Something went wrong";
-    $alert_type = "failure";
+    // If form is not submitted, redirect to index.html
+    header("Location: index.html");
+    exit();
 }
-
 ?>
